9db69add57e6e1a7417b2f04af6b9b33ede254dc
Jackie Gleason <jackiegleason@gmail.com>
2017-10-01 21:23:49 -0400

    Now looks to be upgraded (#1)

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,9 @@
+var express = require('express');
+var router = express.Router();
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
+/* GET home page. */
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-};
-exports.java = function(req, res){
-  res.render('java', { title: 'Express' });
-};
-exports.android = function(req, res){
-  res.render('android', { title: 'Express' });
-};
-exports.responsive = function(req, res){
-  res.render('responsive', { title: 'Express' });
-};
-exports.viz = function(req, res){
-  res.render('viz', { title: 'Express' });
-};
+});
+
+module.exports = router;

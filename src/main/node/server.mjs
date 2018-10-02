b2a6c731ff01f1b24dcdf55a5074866f5cc8cbc1
Jackie Gleason <jackiegleason@gmail.com>
2018-10-02 12:48:26 -0400

    Need to remove https

--- src/main/node/server.mjs
+++ src/main/node/server.mjs
@@ -9,14 +9,14 @@ let express = new Application();
 express.app.set("port", '3000');
 let server = http.createServer(express.app);
 server.listen("3000");
-let secure = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, 'src/main/node/key/server.key')),
-  cert: fs.readFileSync(path.join(__dirname, 'src/main/node/key/server.cert'))
-},express.app);
-secure.listen("3001");
+//let secure = https.createServer({
+//  key: fs.readFileSync(path.join(__dirname, 'src/main/node/key/server.key')),
+//  cert: fs.readFileSync(path.join(__dirname, 'src/main/node/key/server.cert'))
+//},express.app);
+//secure.listen("3001");
 let onListening = function(){
   let addr = server.address();
   let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
 }
 server.on('listening', onListening);
-secure.on('listening', onListening);
+//secure.on('listening', onListening);
